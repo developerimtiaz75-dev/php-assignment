4db33c471c5788d3227ae218cc04a2e0617ce804
Developer Imtiaz <developerimtiaz75@gmail.com>
2026-04-10 21:02:13 +0600

    Update task1.php

--- task1.php
+++ task1.php
@@ -1,1 +1,22 @@
+<?php
 
+$users = [" rAHim@gmail.com ", " kArim@outlook.com ", " sUmi@yahoo.com "];
+
+foreach ($users as $user) {
+
+    // Trim spaces
+    $user = trim($user);
+
+    // Split email
+    $parts = explode("@", $user);
+
+    // Name formatting
+    $name = ucfirst(strtolower($parts[0]));
+
+    // Domain extract
+    $domain = $parts[1];
+
+    echo "Name: $name, Domain: $domain\n";
+}
+
+?>
