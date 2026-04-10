41d8bea657c25dde495703bb834c55bad457a216
Developer Imtiaz <developerimtiaz75@gmail.com>
2026-04-10 21:04:14 +0600

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,1 +1,28 @@
+<?php
 
+class Vehicle {
+    protected $brand;
+
+    public function __construct($brand) {
+        $this->brand = $brand;
+    }
+}
+
+class Car extends Vehicle {
+    private $model;
+
+    public function __construct($brand, $model) {
+        parent::__construct($brand);
+        $this->model = $model;
+    }
+
+    public function displayDetails() {
+        echo "This is a " . $this->brand . " " . $this->model . ".";
+    }
+}
+
+// Usage
+$car = new Car("Toyota", "Corolla");
+$car->displayDetails();
+
+?>
