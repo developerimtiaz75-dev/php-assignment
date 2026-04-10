d6957a181f3960839ad1ffc10e382e1c367b487b
Developer Imtiaz <developerimtiaz75@gmail.com>
2026-04-10 21:03:13 +0600

    Add Employee class with salary update functionality

--- task2.php
+++ task2.php
@@ -1,1 +1,26 @@
+<?php
 
+class Employee {
+    private $name;
+    private $salary;
+
+    public function __construct($name, $salary) {
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+    public function updateSalary($amount) {
+        $this->salary += $amount;
+    }
+
+    public function getDetails() {
+        echo "Name: " . $this->name . ", Salary: " . $this->salary;
+    }
+}
+
+// Usage
+$emp = new Employee("Alice", 5000);
+$emp->updateSalary(1000);
+$emp->getDetails();
+
+?>
